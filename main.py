3e3a935b236b9b21dc348d01eb0cdb6258c6f9e2
nsk4 <nejcsk@hotmail.com>
2019-04-20 15:39:57 +0200

    Added XPath extraction.

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
 from regex_extractor import RegExExtractor
+from xpath_extractor import XPathExtractor
 
 if __name__ == "__main__":
     # RTVSLO data extraction
     with open("resources/rtvslo.si/Audi A6 50 TDI quattro_ nemir v premijskem razredu - RTVSLO.si.html",
               encoding="UTF-8") as file:
         data = file.read().replace('\r', '').replace('\n', '')
-        result = RegExExtractor.find_information_rtvslo(data)
-        print(result)
+        # result_regex = RegExExtractor.find_information_rtvslo(data)
+        result_xpath = XPathExtractor.find_information_rtvslo(data)
+        print(result_xpath)
+
     with open("resources/rtvslo.si/Volvo XC 40 D4 AWD momentum_ suvereno med najboljše v razredu - RTVSLO.si.html",
               encoding="UTF-8") as file:
         data = file.read().replace('\r', '').replace('\n', '')
-        result = RegExExtractor.find_information_rtvslo(data)
+        # result_regex = RegExExtractor.find_information_rtvslo(data)
+        result_xpath = XPathExtractor.find_information_rtvslo(data)
+        print(result_xpath)
 
     # Overstock data extraction
     with open("resources/overstock.com/jewelry01.html") as file:
         data = file.read().replace('\r', '').replace('\n', '')
-        result = RegExExtractor.find_information_overstock(data)
-        print(result)
+        result_regex = RegExExtractor.find_information_overstock(data)
+        result_xpath = XPathExtractor.find_information_overstock(data)
+        print(result_xpath)
+        
     with open("resources/overstock.com/jewelry02.html") as file:
         data = file.read().replace('\r', '').replace('\n', '')
-        result = RegExExtractor.find_information_overstock(data)
-        print(result)
+        result_regex = RegExExtractor.find_information_overstock(data)
+        result_xpath = XPathExtractor.find_information_overstock(data)
+        print(result_xpath)
--- xpath_extractor.py
+++ xpath_extractor.py
@@ -1,3 +1,43 @@
+from lxml.html.soupparser import fromstring
+
+import utilities
+
 
 class XPathExtractor:
-    pass
+    @staticmethod
+    def find_information_rtvslo(website):
+        tree = fromstring(website)
+        return {
+            "Title": tree.xpath("//h1/text()"),
+            "Subtitle": tree.xpath("//div[@class='subtitle']/text()"),
+            "Author": tree.xpath("//div[@class='author-name']/text()"),
+            "PublishedTime": utilities.clean_text(tree.xpath("//div[@class='publish-meta']/text()")[0]),
+            "Lead": tree.xpath("//p[@class='lead']/text()"),
+            "Content": utilities.clean_text(
+                (" ".join(tree.xpath("//div[@class='article-body']//*[not(name()='script')]/text()"))))
+        }
+
+    @staticmethod
+    def find_information_overstock(website):
+        tree = fromstring(website)
+        results = []
+
+        titles = tree.xpath("//td[@valign='top']/a/b//text()")
+        price_parts = tree.xpath("//tr/td[@align='left' and @nowrap='nowrap']//text()")
+        list_prices = price_parts[0::3]
+        prices = price_parts[1::3]
+        savings = price_parts[2::3]
+        contents = tree.xpath("//td[@valign='top']/span[@class='normal']/text()")
+
+        for i in range(len(titles)):
+            results.append(
+                {
+                    "Title": titles[i],
+                    "ListPrice": list_prices[i],
+                    "Price": prices[i],
+                    "Saving": utilities.clean_text(utilities.extract_data(savings[i], "$", " ")),
+                    "SavingPercent": utilities.clean_text(
+                        utilities.extract_data(savings[i], "(", ")")).replace("(", "").replace(")", ""),
+                    "Content": contents[i]
+                })
+        return results
